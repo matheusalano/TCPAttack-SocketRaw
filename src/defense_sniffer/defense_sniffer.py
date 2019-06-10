19dec4396ec4e858b6fe6b5cc218148869654ea1
wosantos@thoughtworks.com <wosantos@thoughtworks.com>
2019-06-10 19:40:08 -0300

    Vai lá magrão

--- src/defense_sniffer/defense_sniffer.py
+++ src/defense_sniffer/defense_sniffer.py
@@ -11,7 +11,14 @@ def monitoring():
         eth_header  = headers[0]
         ip_header   = headers[1]
         tcp_header  = headers[2]
+        
+        other_host = ''
+        if ip_header["src_ip"] == MY_HOST_IP == True:
+            other_host = ip_header["dst_ip"]
+        else:
+            other_host = ip_header["src_ip"]
 
+        ## identificar pacotes de resposta
         if ip_header["src_ip"] == MY_HOST_IP:
             print("ENVIANDO RESPOSTAS")
             flag = tcp_flag(int(tcp_header["tcp_flag"]))            
